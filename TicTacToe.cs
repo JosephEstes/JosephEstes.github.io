276cb68280497ac1e8317273168bf826e2bfb912
Joseph <54252110+josephestes@users.noreply.github.com>
2022-12-08 01:19:28 -0500

    Create TicTacToe.cs

---
+++ TicTacToe.cs
@@ -0,0 +1,121 @@
+using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the game board
+            string[,] gameBoard = new string[3, 3]
+            {
+                {" ", " ", " "},
+                {" ", " ", " "},
+                {" ", " ", " "}
+            };
+
+            // Print the initial game board
+            Console.WriteLine("Tic-Tac-Toe\n");
+            PrintGameBoard(gameBoard);
+
+            // Play the game until it ends
+            string currentPlayer = "X";
+            int remainingMoves = 9;
+            while (remainingMoves > 0)
+            {
+                // If it's the computer's turn, make a move
+                if (currentPlayer == "O")
+                {
+                    MakeComputerMove(gameBoard);
+                    remainingMoves--;
+
+                    // Print the updated game board
+                    PrintGameBoard(gameBoard);
+
+                    // Check if the game has been won
+                    if (HasPlayerWon(gameBoard, currentPlayer))
+                    {
+                        Console.WriteLine("The computer wins!");
+                        break;
+                    }
+                }
+                else
+                {
+                    // Prompt the player to make a move
+                    Console.WriteLine("Enter your move (row,column):");
+
+                    // Read the player's move
+                    int row = int.Parse(Console.ReadLine());
+                    int column = int.Parse(Console.ReadLine());
+
+                    // Make sure the move is valid
+                    if (row < 0 || row > 2 || column < 0 || column > 2 || gameBoard[row, column] != " ")
+                    {
+                        Console.WriteLine("Invalid move. Try again.");
+                        continue;
+                    }
+
+                    // Make the move and update the game state
+                    gameBoard[row, column] = currentPlayer;
+                    remainingMoves--;
+
+                    // Print the updated game board
+                    PrintGameBoard(gameBoard);
+
+                    // Check if the game has been won
+                    if (HasPlayerWon(gameBoard, currentPlayer))
+                    {
+                        Console.WriteLine("You win!");
+                        break;
+                    }
+                }
+
+                // Switch to the other player
+                currentPlayer = (currentPlayer == "X" ? "O" : "X");
+            }
+
+            // If the game ended without a winner, it must be a draw
+            if (remainingMoves == 0)
+            {
+                Console.WriteLine("The game ends in a draw.");
+            }
+        }
+
+        static void PrintGameBoard(string[,] gameBoard)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(gameBoard[i, j]);
+                    if (j < 2)
+                    {
+                        Console.Write("|");
+                    }
+                }
+                Console.WriteLine();
+                if (i < 2)
+                {
+                    Console.WriteLine("-+-+-");
+                }
+            }
+        }
+
+        static void MakeComputerMove(string[,] gameBoard)
+{
+    // Find the first empty square
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (gameBoard[i, j] == " ")
+            {
+                // Make the move
+                gameBoard[i, j] = "O";
+
+                // Return from the method
+                return;
+            }
+        }
+    }
+}
